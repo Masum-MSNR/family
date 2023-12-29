e69f501c1f8c10ac6042bf79422901cd3b7c0577
Nurullah Al-Masum <nam.cse.ruet@gmail.com>
2023-12-29 14:47:26 +0600

    init

---
+++ lib/consts/c_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  static const Color cyan = Color(0xff02bcd4);
+  static const Color blue = Color(0xff0080b4);
+  static const Color red = Color(0xfffe3856);
+  static const Color navyBlue = Color(0xff15284f);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  static const Color transparent = Colors.transparent;
+}
